3efe3c76a98e6e8bd28fd2bdcc25364f2a1e607b
Wood6563 <arparnasing@gmail.com>
2026-03-09 20:26:06 +0530

    Update build.sh

--- x11-packages/dwm/build.sh
+++ x11-packages/dwm/build.sh
@@ -3,15 +3,12 @@ TERMUX_PKG_DESCRIPTION="A dynamic window manager for X"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.8"
-TERMUX_PKG_SRCURL="https://dl.suckless.org/dwm/dwm-$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SRCURL="https://github.com/WOOD6563/Baka/releases/download/Dwm8/dwm-6.8.tar.gz"
 TERMUX_PKG_SHA256=bcf540589ad174d4073f4efa658828411e2f5ba63196cfaf6b71363700f590b7
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="dmenu, fontconfig, libx11, libxft, libxinerama, st"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	wget -O autostart.diff \
-		https://dwm.suckless.org/patches/autostart/dwm-autostart-20161205-bb3bd6f.diff
-	patch -p1 < autostart.diff
 	cp config.def.h config.h
 }
