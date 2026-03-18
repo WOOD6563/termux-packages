cd544f2bc3e6a4ed58f04d5770f84b0e150f9f05
[AWOOD6563 <arparnasing@gmail.com>
2026-03-18 22:02:04 +0530

    '

--- packages/mesa/build.sh
+++ packages/mesa/build.sh
@@ -3,25 +3,20 @@ TERMUX_PKG_DESCRIPTION="An open-source implementation of the OpenGL specificatio
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="docs/license.rst"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="26.0.2"
-TERMUX_PKG_SRCURL=https://archive.mesa3d.org/mesa-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=973f535221be211c6363842b4cce9ef8e9b3e1d5ea86c5450ca86060163c7346
+TERMUX_PKG_VERSION="25.1.4"
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libdrm, libglvnd, libllvm (<< $TERMUX_LLVM_NEXT_MAJOR_VERSION), libwayland, libx11, libxext, libxfixes, libxshmfence, libxxf86vm, ncurses, vulkan-loader, zlib, zstd"
+TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libdrm, libglvnd, libllvm (<< ${_LLVM_MAJOR_VERSION_NEXT}), libwayland, libx11, libxext, libxfixes, libxshmfence, libxxf86vm, ncurses, vulkan-loader, zlib, zstd"
 TERMUX_PKG_SUGGESTS="mesa-dev"
-TERMUX_PKG_BUILD_DEPENDS="libclc, libwayland-protocols, libxrandr, llvm, llvm-tools, mlir, spirv-tools, xorgproto"
+TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols, libxrandr, llvm, llvm-tools, mlir, xorgproto"
 TERMUX_PKG_BREAKS="osmesa, osmesa-demos"
-TERMUX_PKG_CONFLICTS="libmesa, ndk-sysroot (<= 25b), osmesa"
 TERMUX_PKG_REPLACES="libmesa, osmesa"
 
 # FIXME: Set `shared-llvm` to disabled if possible
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --cmake-prefix-path $TERMUX_PREFIX
--Dgbm=enabled
--Dopengl=true
+-Dgbm=enabled                                                               -Dopengl=true
 -Degl=enabled
--Degl-native-platform=x11
--Dgles1=disabled
+-Degl-native-platform=x11                                                   -Dgles1=disabled
 -Dgles2=enabled
 -Dglx=dri
 -Dllvm=enabled
@@ -33,31 +28,18 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dxmlconfig=disabled
 "
 
+termux_pkg_auto_update() {
+	read -r latest < <(curl -fsSL "https://archive.mesa3d.org/" | sed -rn 's/.*mesa-([0-9]+(\.[0-9]+)*).*/\1/p' | sort -Vr);
+	termux_pkg_upgrade_version "${latest}"
+}
+
 termux_step_post_get_source() {
 	# Do not use meson wrap projects
 	rm -rf subprojects
 }
 
 termux_step_pre_configure() {
-	if [ "$TERMUX_PKG_API_LEVEL" -lt 29 ]; then
-		# ELF TLS is supported starting with API level 29.
-		patch --silent -p1 < "$TERMUX_PKG_BUILDER_DIR/0011-lld-undefined-version.diff"
-	fi
-
 	termux_setup_cmake
-	termux_setup_rust
-
-	: "${CARGO_HOME:=${HOME}/.cargo}"
-	export CARGO_HOME
-
-	cargo install --force --locked bindgen-cli
-	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
-		export BINDGEN_EXTRA_CLANG_ARGS="--sysroot ${TERMUX_STANDALONE_TOOLCHAIN}/sysroot"
-		case "${TERMUX_ARCH}" in
-		arm) BINDGEN_EXTRA_CLANG_ARGS+=" --target=arm-linux-androideabi${TERMUX_PKG_API_LEVEL}" ;;
-		*) BINDGEN_EXTRA_CLANG_ARGS+=" --target=${TERMUX_ARCH}-linux-android${TERMUX_PKG_API_LEVEL}" ;;
-		esac
-	fi
 
 	CPPFLAGS+=" -D__USE_GNU"
 	LDFLAGS+=" -landroid-shmem"
@@ -70,9 +52,9 @@ termux_step_pre_configure() {
 			> $_WRAPPER_BIN/cmake
 		chmod 0700 $_WRAPPER_BIN/cmake
 		termux_setup_wayland_cross_pkg_config_wrapper
+		export LLVM_CONFIG="$TERMUX_PREFIX/bin/llvm-config"
 	fi
-	export LLVM_CONFIG="${TERMUX_PREFIX}/bin/llvm-config"
-	export PATH="${_WRAPPER_BIN}:${CARGO_HOME}/bin:${PATH}"
+	export PATH="$_WRAPPER_BIN:$PATH"
 
 	local _vk_drivers="swrast"
 	if [ $TERMUX_ARCH = "arm" ] || [ $TERMUX_ARCH = "aarch64" ]; then
@@ -108,7 +90,4 @@ termux_step_post_make_install() {
 	# Create symlinks
 	ln -sf libEGL_mesa.so ${TERMUX_PREFIX}/lib/libEGL_mesa.so.0
 	ln -sf libGLX_mesa.so ${TERMUX_PREFIX}/lib/libGLX_mesa.so.0
-	ln -sf libRusticlOpenCL.so ${TERMUX_PREFIX}/lib/libRusticlOpenCL.so.1
-
-	unset BINDGEN_EXTRA_CLANG_ARGS LLVM_CONFIG
 }
